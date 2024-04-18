73a9cb0f071fed4ea83e25dc7d566143c84a49f0
mtaromi <mtaromirad@gmail.com>
2024-04-18 15:35:29 +0200

    more tests

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -21,5 +21,15 @@ namespace Prime.UnitTests.Services
 
             Assert.That(result, Is.False, "1 should not be prime");
         }
+
+	[TestCase(-1)]
+	[TestCase(0)]
+	[TestCase(1)]
+	public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+	{
+    		var result = _primeService?.IsPrime(value);
+
+    		Assert.That(result, Is.False, $"{value} should not be prime");
+	}
     }
 }
